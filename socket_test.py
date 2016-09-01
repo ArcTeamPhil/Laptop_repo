303c28a70b26bb36c083317a723459ae0477aad0
ArcTeamPhil <phil.repisky@gmail.com>
2016-09-01 08:56:01 -0400

    Commit socket file

---
+++ socket_test.py
@@ -0,0 +1,183 @@
+import numpy as np
+import cv2
+import scipy
+
+## http://www.binarytides.com/python-socket-programming-tutorial/
+'''
+#Socket client example in python
+ 
+import socket   #for sockets
+ 
+#create an AF_INET, STREAM socket (TCP)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+print 'Socket Created'
+'''
+'''
+#handling errors in python socket programs
+ 
+import socket   #for sockets
+import sys  #for exit
+ 
+try:
+    #create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit();
+ 
+print 'Socket Created'
+'''
+'''
+import socket   #for sockets
+import sys  #for exit
+ 
+try:
+    #create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit();
+ 
+print 'Socket Created'
+ 
+host = 'www.google.com'
+ 
+try:
+    remote_ip = socket.gethostbyname( host )
+ 
+except socket.gaierror:
+    #could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+     
+print 'Ip address of ' + host + ' is ' + remote_ip
+'''
+'''
+import socket   #for sockets
+import sys  #for exit
+ 
+try:
+    #create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit();
+ 
+print 'Socket Created'
+ 
+host = 'www.google.com'
+port = 80
+ 
+try:
+    remote_ip = socket.gethostbyname( host )
+ 
+except socket.gaierror:
+    #could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+     
+print 'Ip address of ' + host + ' is ' + remote_ip
+ 
+#Connect to remote server
+s.connect((remote_ip , port))
+ 
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+'''
+'''
+import socket   #for sockets
+import sys  #for exit
+ 
+try:
+    #create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit();
+ 
+print 'Socket Created'
+ 
+host = 'www.google.com'
+port = 80
+ 
+try:
+    remote_ip = socket.gethostbyname( host )
+ 
+except socket.gaierror:
+    #could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+     
+print 'Ip address of ' + host + ' is ' + remote_ip
+ 
+#Connect to remote server
+s.connect((remote_ip , port))
+ 
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+ 
+#Send some data to remote server
+message = "GET / HTTP/1.1\r\n\r\n"
+ 
+try :
+    #Set the whole string
+    s.sendall(message)
+except socket.error:
+    #Send failed
+    print 'Send failed'
+    sys.exit()
+ 
+print 'Message send successfully'
+'''
+
+#Socket client example in python
+ 
+import socket   #for sockets
+import sys  #for exit
+ 
+#create an INET, STREAMing socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+     
+print 'Socket Created'
+ 
+host = 'www.google.com';
+port = 80;
+ 
+try:
+    remote_ip = socket.gethostbyname( host )
+ 
+except socket.gaierror:
+    #could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+ 
+#Connect to remote server
+s.connect((remote_ip , port))
+ 
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+ 
+#Send some data to remote server
+message = "GET / HTTP/1.1\r\n\r\n"
+ 
+try :
+    #Set the whole string
+    s.sendall(message)
+except socket.error:
+    #Send failed
+    print 'Send failed'
+    sys.exit()
+ 
+print 'Message send successfully'
+ 
+#Now receive data
+reply = s.recv(4096)
+ 
+print reply
+
+## close socket
+s.close()
+
+
